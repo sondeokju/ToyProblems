62b6199acaabb2f69236dc784dfa3dededf98428
sondeokju <thsejrwn7808@gmail.com>
2018-08-10 09:30:56 +0900

    hashTable

--- toy/hashTable.js
+++ toy/hashTable.js
@@ -7,21 +7,70 @@ var makeHashTable = function(){
     var result = {};
     var storage = [];
     var storageLimit = 1000;
-    result.insert = function(/*...*/ 
-  ){
-      // TODO: implement `insert()`
+    result.insert = function(key, value 
+  ){     
+        var index = this.getIndexBelowMaxForKey(key, this.storageLimit); 
+        var bucket = this._storage[index];
+  
+        if (!bucket) {
+          var bucket = [];
+          this._storage[index] = bucket;
+        }
+      
+        var override = false;
+  
+        for (var i = 0; i < bucket.length; i++) {
+          var tuple = bucket[i];
+          if (tuple[0] === key) {       
+            tuple[1] = value;
+            override = true;
+          }
+        }
+      
+        if (!override) {
+          bucket.push([key, value]);
+          this._count++
+        }
+        return this;
     };
   
-    result.retrieve = function(/*...*/ 
+    result.retrieve = function(key
   ){
-      // TODO: implement `retrieve()`
+      var index = this.getIndexBelowMaxForKey(key, this.storageLimit);
+      var bucket = this._storage[index];
+  
+      if (!bucket) {
+        return null;
+      }
+  
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
+          return tuple[1];
+        }
+      }
+      return null;
     };
   
-    result.remove = function(/*...*/ 
+    result.remove = function(key
   ){
-      // TODO: implement `remove()`
-    };
+      var index = this.getIndexBelowMaxForKey(key, this.storageLimit);
+      var bucket = this._storage[index];
+      if (!bucket) {
+        return null;
+      }
   
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+   
+        if (tuple[0] === key) {
+          bucket.splice(i, 1);
+          this._count--;       
+          return tuple[1];
+        }
+      }
+    };
+    
     return result;
   };
   
